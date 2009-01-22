5cfd3adcc8004af12fe8cbe2bbb221ef29616e70
Scott Barron <scott@elitists.net>
2009-01-22 17:37:44 -0500

    start tcx parser - interaction with nokogiri

--- lib/garmin/tcx_parser.rb
+++ lib/garmin/tcx_parser.rb
@@ -1,5 +1,17 @@
+require 'rubygems'
+require 'nokogiri'
+
 module Garmin
   class TcxParser
-    
+    def initialize(file)
+      @file = file
+    end
+
+    def parse
+      f = File.open(@file)
+      doc = Nokogiri.XML(f.read)
+
+      f.close
+    end
   end
 end
--- test/test_tcx_parser.rb
+++ test/test_tcx_parser.rb
@@ -3,7 +3,45 @@ require 'test/garmin_test_setup'
 class TestTcxParser < Test::Unit::TestCase
   context "existance" do
     should "exist" do
-      assert Garmin::TcxParser.new
+      assert Garmin::TcxParser.new('foo.tcx')
+    end
+  end
+
+  context "parsing" do
+    setup do
+      @parser = Garmin::TcxParser.new('foo.tcx')
+    end
+    
+    should "open the file" do
+      flexmock(File).should_receive(:open).with('foo.tcx').and_return(flexmock('file', :read => '', :close => '')).once
+
+      @parser.parse
+    end
+
+    should "read the file" do
+      file = flexmock('file', :close => '')
+      file.should_receive(:read).once
+      
+      flexmock(File).should_receive(:open).with('foo.tcx').and_return(file)
+
+      @parser.parse
+    end
+
+    should "pass the file data into nokogiri" do
+      flexmock(File).should_receive(:open).with('foo.tcx').and_return(flexmock('file', :read => 'xml data', :close => ''))
+
+      flexmock(Nokogiri).should_receive(:XML).with('xml data').once
+
+      @parser.parse
+    end
+
+    should "close the file handle" do
+      file = flexmock('file', :read => '')
+      file.should_receive(:close).once
+      
+      flexmock(File).should_receive(:open).with('foo.tcx').and_return(file)
+
+      @parser.parse
     end
   end
 end
