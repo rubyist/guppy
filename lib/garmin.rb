6f6d621fe2228f656cfc9aabe6007e55b4d45eca
Scott Barron <scott@elitists.net>
2009-01-23 13:13:23 -0500

    remove garmin/ from lib dir - don't really need it

--- lib/garmin.rb
+++ lib/garmin.rb
@@ -1,8 +1,8 @@
-require File.join(File.dirname(__FILE__), 'garmin', 'activity')
-require File.join(File.dirname(__FILE__), 'garmin', 'track_point')
-require File.join(File.dirname(__FILE__), 'garmin', 'tcx_parser')
-require File.join(File.dirname(__FILE__), 'garmin', 'gpx_parser')
-require File.join(File.dirname(__FILE__), 'garmin', 'db')
+require File.join(File.dirname(__FILE__), 'activity')
+require File.join(File.dirname(__FILE__), 'track_point')
+require File.join(File.dirname(__FILE__), 'tcx_parser')
+require File.join(File.dirname(__FILE__), 'gpx_parser')
+require File.join(File.dirname(__FILE__), 'db')
 
 module Garmin
   TCX = 'tcx'
