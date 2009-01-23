9197f8231fcc0e05fd4f4126539a15c43493d0de
Scott Barron <scott@elitists.net>
2009-01-22 20:09:59 -0500

    fix Garmin::Db.open to work right

--- lib/garmin/db.rb
+++ lib/garmin/db.rb
@@ -12,12 +12,24 @@ module Garmin
     end
     
     def self.open(file_name)
-      case file_type(file_name)
+      db = new(file_name)
+      db.parse
+      return db
+    end
+
+    def initialize(file)
+      @file_name = file
+    end
+    
+    def parse
+      case self.class.file_type(@file_name)
       when TCX
-        Garmin::TcxParser.new(file_name)
+        @doc = Garmin::TcxParser.new(@file_name)
       when GPX
-        Garmin::GpxParser.new(file_name)
+        @doc = Garmin::GpxParser.new(@file_name)
       end
     end
   end
 end
+
+# Db.open('file') => chooses parser, parses, new db
--- test/test_db.rb
+++ test/test_db.rb
@@ -3,7 +3,7 @@ require 'test/garmin_test_setup'
 class TestDb < Test::Unit::TestCase
   context "exist" do
     should "exist" do
-      assert Garmin::Db.new
+      assert Garmin::Db.new('file.tcx')
     end
   end
   context "determining file type" do
@@ -23,14 +23,38 @@ class TestDb < Test::Unit::TestCase
   end
 
   context "opening database files" do
+    should "create a new Db object" do
+      flexmock(Garmin::Db).should_receive(:new).with('foo.tcx').and_return(flexmock('db', :parse => '')).once
+
+      Garmin::Db.open('foo.tcx')
+    end
+    
+    should "parse it" do
+      db = flexmock('db')
+      db.should_receive(:parse).once
+      
+      flexmock(Garmin::Db).should_receive(:new).with('foo.tcx').and_return(db)
+
+      Garmin::Db.open('foo.tcx')
+    end
+    
+    should "return the Db object" do
+      db = flexmock('db', :parse => '')
+      flexmock(Garmin::Db).should_receive(:new).with('foo.tcx').and_return(db)
+      
+      assert_equal db, Garmin::Db.open('foo.tcx')
+    end
+  end
+
+  context "parsing" do
     should "use TcxParser for tcx files" do
       flexmock(Garmin::TcxParser).should_receive(:new).with('foo.tcx').once
-      Garmin::Db.open('foo.tcx')
+      Garmin::Db.new('foo.tcx').parse
     end
 
     should "use GpxParser for gpx files" do
       flexmock(Garmin::GpxParser).should_receive(:new).with('foo.gpx').once
-      Garmin::Db.open('foo.gpx')
+      Garmin::Db.new('foo.gpx').parse
     end
   end
 end
